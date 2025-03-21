2f79de965955a3dfe31203ffc84fbd230d2f09b8
Xuelin Cui <xuelincui@gmail.com>
2025-03-21 18:16:42 -0400

    leetcode commit

---
+++ leetcode/LongestCommonPrefix14/main.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+
+string longestCommonPrefix(vector<string>& strs)
+{
+    for (int i = 0; i < strs.size(); i++)
+    {
+
+    }
+}
+
+int main()
+{
+    std::vector<string> strs;
+
+    longestCommonPrefix( strs);
+
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
