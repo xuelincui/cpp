3c919c975432fabf473f45cd3d28b7cfeb42df2c
Xuelin Cui <xuelincui@gmail.com>
2025-03-25 09:45:49 -0400

    continuw working on 150 problems

--- leetcode/LongestCommonPrefix14/main.cpp
+++ leetcode/LongestCommonPrefix14/main.cpp
@@ -1,21 +1,43 @@
 #include <iostream>
 #include <vector>
 #include <string>
+#include <climits>
 
 using namespace std;
 
 
 string longestCommonPrefix(vector<string>& strs)
 {
-    for (int i = 0; i < strs.size(); i++)
-    {
+    string comm = "";
+    int min_len = INT_MAX;
 
+    for (string s: strs)
+    {
+        if (s.length() < min_len)
+        {
+            min_len = s.length();
+        }
+    }
+
+    int i = 0;
+    bool still_same = true;
+
+    while ( still_same && i < min_len)
+    {
+        char c = strs[0][i];
+        for (int n = 1; n < strs.size(); n++)
+        {
+            if (strs[n][0] !=c)
+            {
+                still_same = false;
+            }
+        }
     }
 }
 
 int main()
 {
-    std::vector<string> strs;
+    std::vector<string> strs = {"flower","flow","flight"};
 
     longestCommonPrefix( strs);
 
